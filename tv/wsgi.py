a714252a909277ee449ce2ce36d31658e7de031d
nahuelcace <nahue_cge@hotmail.com>
2022-09-18 13:55:58 -0300

    config heroku

--- tv/wsgi.py
+++ tv/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tv.prod')
 
 application = get_wsgi_application()
