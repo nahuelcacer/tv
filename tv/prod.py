fd273e9da652c252f36e3e050c42b5a0104aa653
nahuelcace <nahue_cge@hotmail.com>
2022-09-18 13:52:58 -0300

    config heroku

--- tv/prod.py
+++ tv/prod.py
@@ -7,7 +7,14 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd5amk8v9jdiugj',
+        'USER': 'lhznimgisoodlh',
+        'PASSWORD': 'a8c8597ce5aa83737736e5fc310d7bfe820bea7714ee429870fa251c23fa1c6a',
+        'HOST': 'ec2-34-200-205-45.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+import django_heroku
+django_heroku.settings(locals())
