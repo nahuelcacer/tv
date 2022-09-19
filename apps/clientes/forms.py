ef22cc3b95e66ed897272ea60b44ae0729987bb6
nahuelcace <nahue_cge@hotmail.com>
2022-09-19 02:16:25 -0300

    clientes

--- apps/clientes/forms.py
+++ apps/clientes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Cliente
 class Cliente(forms.ModelForm):
-    
+    fecha_de_alta = forms.DateField()
     class Meta:
         model = Cliente
         fields = ['nombre', 'telefono', 'direccion', 'fecha_de_alta']
