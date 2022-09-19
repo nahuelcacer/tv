2120366db11ca85a0888ed8446ce9d3d5d47935a
Nahuel <nahue_cge@hotmail.com>
2022-09-19 15:18:31 -0300

    clientes

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 #from .views import
 app_name = 'apps.usuario'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="usuario/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+
 ]
