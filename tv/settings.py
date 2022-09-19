842a303df8f5ecbc1c0500534750585fdd398f1a
nahuelcace <nahue_cge@hotmail.com>
2022-09-19 01:50:06 -0300

    clientes

--- tv/settings.py
+++ tv/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap3',
+    
     'apps.usuario',
     'apps.clientes'
 ]
